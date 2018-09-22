aea6ce2408b45602e736e826845c4a5a0aa93b55
William Sampson <wsampson@csumb.edu>
2018-09-22 04:57:28 +0000

    Bugfixes, added arguments to printTable and getWinner

--- index.php
+++ index.php
@@ -13,10 +13,9 @@
     
     <?php
       function play(){
-          
         //Set up deck order and cards array
         $shuff = array();//$shuff determines the order in which cards will be drawn
-        for($i = 0; $i < 52; $i++)
+        for($i = 1; $i <= 52; $i++)
           $shuff[] = $i;
         var_dump($shuff);//test line
         echo "<br><br>";
@@ -27,41 +26,40 @@
           $shuff[$i] = $shuff[$j];
           $shuff[$j] = $temp;
         }
-        /*var_dump($shuff);//test line
-        echo "<br><br>";*/
+        
+        var_dump($shuff);//test line
+        echo "<br><br>";
         
         $cards = array();//$cards implements value & suit for each card
         //Find the info for a card by knowing its number, applying divison and mod
-        //suit: clubs = 0, diamonds = 1, hearts = 2, spades = 3
-        //value: 1-13 maps to Ace through King
+        //suit: clubs = 0, diamonds = 1, hearts = 2, spades = 3 --> Translates to folder names
+        //value: 1-13 maps to Ace through King --> Translates to .png names
         for($i = 0; $i < 52; $i++)
           $cards["$i"] = array("value"=>$i%13+1, "suit"=>(int)($i/13));
-        /*var_dump($cards);//test line
-  
-        echo "<br><br>";*/
+          
+        var_dump($cards);//test line
+        echo "<br><br>";
         
-  
         $playerNames = array("Thanos", "Pepe", "Ainz", "God"); //player names
 
         $playerHands = array("Thanos"=>array(), "Pepe"=>array(),"Ainz"=>array(),"God"=>array());
         
         //Deal hands
         $shuffCount = 0;
-        for($i = 0; $i < 4; $i++){
-          while(getScore($playerHands[$playerNames[$i]]) < 37){
+        for($i = 0; $i < 4; $i++){//For each player, draw cards until cutoff
+          /*while(getScore($playerHands[$playerNames[$i]]) < 37){//THESE LINES PREVENT PAGE FROM LOADING
             array_push($playerHands[$playerNames[$i]],$cards[$shuff[$shuffCount]]);
             $shuffCount++;
-          }
+          }*/
+          echo "Player $playerNames[$i] draws cards";//Test line
         }
               
         //Print results
         //Print table of players and their scores
-        printTable();
+        printTable($playerNames, $playerHands);
         
-        getWinner();
+        getWinner($playerNames, $playerHands);
         
-
-    
       }
       
       
@@ -69,14 +67,13 @@
         echo "<img src= 'img/$suit/$value' alt = '$suit $value'>";
       }
       
-      
       function showHand($hand){
         for($i = 0; $i < count($hand); $i++){  //iterate through whole hand
           showCard($hand[$i["value"]], $hand[$i["suit"]]);  //print each card in hand
         }
       }
       
-      function getScore($hand){//NEED TO FIX FOREACH LOOPS
+      function getScore($hand){
         $score = 0;
         for($i = 0; i < count($hand); $i++){
           $score += $hand[$i["value"]];
@@ -88,7 +85,7 @@
         echo "<figure><img src='img/players/$name.png' alt = 'player $name'><figcaption>$name</figcaption></figure>"; //show player image with name below them
       }
       
-      function getWinner(){
+      function getWinner($playerNames, $playerHands){
         $scores = array();
         for($i=0;$i<4;$i++){
           array_push($scores, getScore($player.$i));
@@ -105,7 +102,7 @@
         
       }
       
-      function printTable(){
+      function printTable($playerNames, $playerHands){//INFINITE LOOP OF PEPE
         for($i=0;$i<4;$i++){
           echo "<div class='col'>"; //one player per loop
             //player icon and name, to be on the left
@@ -127,15 +124,13 @@
         }  
       }
       
-      
       //Play Silverjack
       $time_start = microtime(true); //Start of script run
+      
       play();
-
-      //Todo: calculate time to process and display it
       
       $time_end = microtime(true);  //end of script run
-      $execution_time = ($time_end - $time_start)/60; //get executiion time in minutes and seconds
+      $execution_time = ($time_end - $time_start)/60; //get execution time in minutes and seconds
       echo "<h3>Execution Time:" . $execution_time . "Mins</h3> <br><br>";  //Print execution time
     ?>
     
